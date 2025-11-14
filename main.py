f60a360527914719cc3a9afe10961b399bc5e80c
terimakichutmadharchodrandikeaulad <163394065+terimakichutmadharchodrandikeaulad@users.noreply.github.com>
2025-11-14 10:19:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,12 @@ import pytz
 
 #Logging setup
 
-logging.basicConfig( format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO ) logger = logging.getLogger(name)
+# Logging setup
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO
+)
+logger = logging.getLogger(__name__)
 
 MongoDB Configuration
 
